2851655a93b4edb2c76599d7683ff1a12bc5ca14
Ahmed Saleh <a.m0507979@gmail.com>
2025-10-09 15:04:34 +0300

    s

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import About from "@/components/About";
+import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
+import Navigation from "@/components/Navigation";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
+import Contact from "@/components/Contact";
+
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <Navigation />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </main>
+  );
+}
